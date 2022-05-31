311a393f8b6e2a70c3fb5627ade223a64421795b
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-05-31 14:48:53 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -111,7 +111,7 @@ Blockly.Python["ds1307_settime"] = function (block) {
 Blockly.Blocks["ds1307_gettime"] = {
   init: function() {
     this.jsonInit({
-      message0: " lấy giá trị %1 từ cảm biến",
+      message0: Msg.BLOCK_DS1307_GETTIME_MESSAGE0,
       args0: [
         {
           type: "field_dropdown",
