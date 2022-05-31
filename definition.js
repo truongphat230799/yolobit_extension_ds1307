8485ca68c4d0b229a09b02f117a4a62e316307ce
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-05-31 16:24:46 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -1,7 +1,7 @@
 Blockly.Blocks["ds1307_showtime"] = {
   init: function() {
     this.jsonInit({
-      message0: Blockly.Msg.BLOCK_DS1307_SHOWTIME_MESSAGE0,
+      message0: "lấy giá trị thời gian từ cảm biến",
       args0: [
       ],
       colour: "#CFF600",
