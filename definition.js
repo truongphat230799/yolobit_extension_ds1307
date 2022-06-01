48410ae1725f8d79dec56cc954542bc423ebcefa
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-06-01 14:18:29 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -133,22 +133,21 @@ Blockly.Python["ds1307_gettime"] = function(block) {
   Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin, SoftI2C';
   Blockly.Python.definitions_["import_ds1307"] = "from ds1307 import DS1307";
   Blockly.Python.definitions_["import_create_ds1307"] = "ds1307 = DS1307(SoftI2C(scl=Pin(22), sda=Pin(21)))";
-  var x = "gettime = ds1307.datetime()";
   var code = "";
   if (dropdown_data == "YEAR")
-    code = "gettime[0]\n";
+    code = "ds1307.datetime()[0];
   else if (dropdown_data == "MONTH")
-    code =  "gettime[1]\n";
+    code =  "ds1307.datetime()[1]\n";
   else if (dropdown_data == "DATE")
-    code =  "gettime[2]\n"; 
+    code =  "ds1307.datetime[2]\n"; 
   else if (dropdown_data == "WEEKDAY")
-    code = "gettime[3]\n";
+    code = "ds1307.datetime[3]\n";
   else if (dropdown_data == "HOUR")
-    code = "gettime[4]\n";
+    code = "ds1307.datetime[4]\n";
   else if (dropdown_data == " MINUTE")
-    code = "gettime[5]\n";
+    code = "ds1307.datetime[5]\n";
   else if (dropdown_data == "SECOND")
-    code = "gettime[6]\n";
+    code = "ds1307.datetime[6]\n";
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };
