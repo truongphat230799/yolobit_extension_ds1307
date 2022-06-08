ed5574d762e6ed17f95316cf017e1e63127eaaa1
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-06-08 14:33:06 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -82,7 +82,7 @@ Blockly.Python["ds1307_settime"] = function (block) {
 Blockly.Blocks["ds1307_gettime"] = {
   init: function() {
     this.jsonInit({
-      message0: " đọc giá trị %1 tử RTC",
+      message0: " đọc giá trị %1 từ RTC",
       args0: [
         {
           type: "field_dropdown",
