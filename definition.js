49c6a00bcb4bc53392f35299a147e585c87cbc58
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-06-08 11:10:05 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -4,7 +4,7 @@ Blockly.Blocks["ds1307_settime"] = {
     this.jsonInit({
       colour: "#006600",
       tooltip: "",
-      message0: "cài đặt giá trị năm %1 %2 tháng %3 %4 ngày %5 %6 giờ %9 %10 phút %11 %12 giây %13 %14",
+      message0: "cài đặt giá trị năm %1 %2 tháng %3 %4 ngày %5 %6 giờ %7 %8 phút %9 %10 giây %11 %12",
       args0: [
         { type: "input_dummy" },
         {
